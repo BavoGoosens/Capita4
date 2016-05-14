c1458de88f2a54d630a489d64d2965eaa21fb91a
BavoGoosens <bavogoosens@hotmail.com>
2016-05-14 15:41:19 +0200

    gepruts + roadmap in code/cool_filename.py

---
+++ code/cool_filename.py
@@ -0,0 +1,18 @@
+# import data set and handle missing values
+
+# choose a random date.
+
+# take 30 historic days before the data
+# keep these stored to plot them later against the trained model
+
+# get next week to get the actual data later to compare the predictions against
+
+# train on historic data.
+
+# plot the trained models against the data they were trained on
+# together with least squares measures(in order to experiment with diff linear models)
+
+# plot the predicted values (by the model) against the actual prices for that week
+# it is this prediction that we'll feed to the scheduler
+
+# Feed the predicted data to the scheduler and calculate the cost
